419c5dc2002dffb6ac5c580ed06d51c6b8cfc0f8
Brad <bleisero57@gmail.com>
2016-01-27 17:19:09 -0800

    Implemented "random" difficulty
    Added random difficulty to test AI implementation

--- ai.c
+++ ai.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <time.h>
 #include "errors.h"
 #include "defs.h"
 #include "board.h"
@@ -18,6 +19,9 @@ int aiMove(int diff, int team, board *board){
 		case(3): /* hard */
 			hardMove(team, board);
 			break;
+		case(4):
+			randomMove(team, board);
+			break;
 		default:
 			printf("Missing difficulty parameter.\n");
 			break;
@@ -32,12 +36,15 @@ void medMove(int team, board *board){
 	
 }
 void hardMove(int team, board *board){
+	
+}
+void randomMove(int team, board *board){
 	int *aiTeamPos = checkPiecePos(team, board);
 	int opTeam = (team == 1) ? 0 : 1;
 	int *opTeamPos = checkPiecePos(opTeam, board);
 	
 	/* check moves cell by cell */
-	int entry = 0;
+	/* int entry = 0; Unused right now, saving just in case
 	while(aiTeamPos){
 		cell *read = getCell(*(aiTeamPos+entry), board)
 		int *aiList = checkAvailMoves(read->piece);
@@ -45,12 +52,36 @@ void hardMove(int team, board *board){
 			break;
 		}
 		entry++;
+	} */
+	
+	srand(time(NULL));
+	int *pieceMoves;
+	cell *moveCell;
+	while(1){ /* Selects a random piece to move, and gets corresponding available moves */
+		int rNum = rand()%17;
+		cell *rCell = getCell(*(aiTeamPos+rNum), board);
+		if(*(aiTeamPos+rNum) != -2){
+			pieceMoves = checkAvailMoves(rCell->piece);
+			moveCell = rCell;
+			break;
+		}
 	}
+	int count = 0;
+	while(1){ /* count number of available moves */
+		if(*(pieceMoves+count) == -2){
+			break;
+		}
+		count++;
+	}
+	int selNum = rand()%count;
+	int move = *(pieceMoves+selNum);
+	cell *nextCell = getCell(move, board);
+	replacePiece(nextCell, moveCell->piece);
 	
 	/* Freeing memory */
-	/* deletecell read, free aiList */
-	free(aiTeamPos);
-	free(opTeamPos);
+	/* deletecell read, free aiList and pieceMoves */
+	free(aiTeamPos); free(opTeamPos);
+	
 }
 
 /* Returns pointer to list of specified team's piece locations */
@@ -74,6 +105,10 @@ int *checkPiecePos(int team, board *board){
 			}
 		}
 	}
-	*(cellList+entry) = -2;
+	int j;
+	for(j=entry; j<17; j++){ /* fills last (and any extra) entries with -2 */
+		*(cellList+entry) = -2; /* this num signals there's nothing left to read */
+	}
 	return cellList;
+	/* cellList will need to be freed at some point */
 }
--- ai.h
+++ ai.h
@@ -7,6 +7,7 @@ int aiMove(int difficulty, int team, board *board);
 void easyMove(int team, board *board);
 void medMove(int team, board *board);
 void hardMove(int team, board *board);
+void randomMove(int team, board *board);
 
 int *checkPiecePos(int team, board *board);
 
